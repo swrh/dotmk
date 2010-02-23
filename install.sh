8d64a7194631482eb37ca35030682860d0b4b8f7
Fernando Silveira <fsilveira@gmail.com>
2010-02-23 09:19:48 -0300

    Added nicer status messages and removed automatic Makefile creation feature.

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
-#!/bin/sh -ex
+#!/bin/sh -e
 
 build()
 {
 	mkfile="$1"
+
+	echo "building \`${MKDIR}/${mkfile}' file..."
 
 	exec 9>&1
 	exec 1>"${MKDIR}/${mkfile}"
@@ -31,65 +33,18 @@ EOF
 	exec 1>&9-
 }
 
-cleandir()
-{
-	dir="${1}"
-
-	find "${dir}" -mindepth 1 -maxdepth 1 -and ! -name .gitignore -print0 | xargs -0 rm -fr
-	mkdir -p "${MKDIR}"
-}
-
-automakefile()
-{
-	makefile="Makefile"
-	prog="`basename "${PWD}"`"
-
-	exec 9>&1
-	exec 1>"${makefile}"
-
-	cat << EOF
-# ${makefile}
-
-PROGS=	${prog}
-
-${prog}_SRCS=	\\
-EOF
-
-	tmpfile="`mktemp "/tmp/${0##*/}-$$.XXXXXX"`"
-	find . -name '*.c' -or -iname '*.cpp' -or -iname '*.cxx' -or -iname '*.c++' > "${tmpfile}"
-	head -n -1 < "${tmpfile}" | sed -e 's/^/\t/;s/$/ \\/'
-	tail -n 1 < "${tmpfile}" | sed -e 's/^/\t/'
-
-	for incdir in ./include; do
-		[ -d "${incdir}" ] || continue
-		echo "${incdir}"
-	done > "${tmpfile}"
-
-	if [ -s "${tmpfile}" ]; then
-		echo
-		echo "${prog}_INCDIRS=	\\"
-		head -n -1 < "${tmpfile}" | sed -e 's/^/\t/;s/$/ \\/'
-		tail -n 1 < "${tmpfile}" | sed -e 's/^/\t/'
-	fi
-
-	cat << EOF
-
-include ${MKDIR}/build.mk
-EOF
-	exec 1>&9-
-
-	rm -f "${tmpfile}"
-
-	cat "${makefile}"
-}
-
 MKDIR="mk"
 TOPDIR="`dirname "${0}"`"
+TOPDIR="`cd "${TOPDIR}"; pwd`"
 SRCDIR="${TOPDIR}/src"
 
-cleandir "${MKDIR}"
+if [ $# -eq 1 ]; then
+	MKDIR="${1}/${MKDIR}"
+fi
+
+mkdir -p "${MKDIR}"
 
 build build.mk
 build subdir.mk
 
-[ "${1}" != "-m" -o -f Makefile ] || automakefile
+echo "dotmk is now installed under the \`${MKDIR}' directory."
