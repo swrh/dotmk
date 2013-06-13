174d9c1fc3970ff4377245b6184a97515647c7d0
Fernando Silveira <fsilveira@gmail.com>
2013-06-13 10:11:45 -0300

    Fixed parallel execution (jobserver) when using the "subdir.mk" module.
    This commit closes issue #3.

--- src/subdir.mk
+++ src/subdir.mk
@@ -9,19 +9,19 @@ define SUBDIR_template
 DEFAULT_TARGETS+=	$(1)
 .PHONY: $(1)
 $(1):
-	@$(MAKE) -C $(1) all
+	@$$(MAKE) -C $(1) all
 
 INSTALL_TARGETS+=	$(1)_install
 $(1)_install:
-	@$(MAKE) -C $(1) install
+	@$$(MAKE) -C $(1) install
 
 CLEAN_TARGETS+=	$(1)_clean
 $(1)_clean:
-	@$(MAKE) -C $(1) clean
+	@$$(MAKE) -C $(1) clean
 
 DISTCLEAN_TARGETS+=	$(1)_distclean
 $(1)_distclean:
-	@$(MAKE) -C $(1) distclean
+	@$$(MAKE) -C $(1) distclean
 endef
 
 $(foreach subdir,$(SUBDIRS),$(eval $(call SUBDIR_template,$(subdir))))
